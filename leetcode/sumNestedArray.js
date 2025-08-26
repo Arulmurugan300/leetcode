46d8f348be646d14df43563a863aac8b5c96a255
Arulmurugan300 <arulmadhujothi@gmail.com>
2025-08-26 17:54:14 +0530

    sample ommit

--- leetcode/sumNestedArray.js
+++ leetcode/sumNestedArray.js
@@ -1,13 +1,13 @@
-const input = [1,[2,[3,[4]],5]];
+const input = [1, [2, [3, [4]], 5]];
 let sumTotal = 0
-function sum(x){
-  for(let i=0; i < x.length; i++){
-    if(x[i].length > 0){
+function sum(x) {
+  for (let i = 0; i < x.length; i++) {
+    if (x[i].length > 0) {
       sum(x[i]);
     }
-    else{
-      sumTotal+=x[i];
-    }
+    else {
+      sumTotal += x[i];
+    };
   }
   return sumTotal;
 }
