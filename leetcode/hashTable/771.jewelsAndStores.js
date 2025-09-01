52689e17719a7e188aa6c35caceb90194d807460
Arulmurugan M <128895636+Arulmurugan300@users.noreply.github.com>
2025-09-01 18:42:43 +0530

    Update 771.jewelsAndStores.js

--- leetcode/hashTable/771.jewelsAndStores.js
+++ leetcode/hashTable/771.jewelsAndStores.js
@@ -2,7 +2,7 @@ var numJewelsInStones = function (jewels, stones) {
   const jewel = new Set(jewels);
   let count = 0
   for (index of stones) {
-    if (jewel.has(index)) {
+    if (jewel.has(index)) { 
       count++;
     }
   } console.log(count)
