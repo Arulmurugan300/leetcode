31f1397abb2a983ca010badb87cc98b4e278e1c4
Arulmurugan M <128895636+Arulmurugan300@users.noreply.github.com>
2025-09-03 23:45:10 +0530

    Update 11areaSumMediumLevel.js
    a

--- leetcode/11areaSumMediumLevel.js
+++ leetcode/11areaSumMediumLevel.js
@@ -2,7 +2,6 @@ var maxArea = function (height) {
   let max = 0;
   let left = 0;
   let right = height.length - 1;
-
   while (left < right) {
     let width = (right - left)
     const area = width * (Math.min(height[left], height[right]));
