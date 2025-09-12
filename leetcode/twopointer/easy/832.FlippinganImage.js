2dc77d7ad730d8f745e3e4a4ecaace4272950a21
Arulmurugan300 <arulmadhujothi@gmail.com>
2025-09-12 21:02:23 +0530

    832. Flipping an Image

---
+++ leetcode/twopointer/easy/832.FlippinganImage.js
@@ -0,0 +1,10 @@
+var flipAndInvertImage = function (image) {
+    for (let i of image) {
+        i.reverse();
+        for (let j = 0; j < i.length; j++) {
+            i[j] = i[j] == 0 ? 1 : 0;
+        }
+    }
+    return image;
+};
+flipAndInvertImage([1, 0, 0], [0, 1, 0], [0, 0, 0])
